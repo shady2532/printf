72bc3cc4066e8407472f2d3b3d164166a586567b
xxyouszz <125680172+xxyouszz@users.noreply.github.com>
2023-04-25 22:50:09 +0200

    Update _printf_datatypes.c

--- _printf_datatypes.c
+++ _printf_datatypes.c
@@ -14,10 +14,9 @@ int print_int(va_list valist, parameters_t *parameter)
 	if (parameter->l_modifier)
 		l = va_arg(valist, long);
 	else if (parameter->h_modifier)
-	        l = (short int)va_arg(valist, int);
-        else
-	        l = (int)va_arg(valist, int);
-	
+		l = (short int)va_arg(valist, int);
+	else
+		l = (int)va_arg(valist, int);
 	return (print_number(convert(l, 10, 0, parameter), parameter));
 }
 
@@ -44,7 +43,7 @@ int print_percent(va_list valist, parameters_t *parameter)
  */
 int print_S(va_list valist, parameters_t *parameter)
 {
-    char *str = va_arg(valist, char *);
+	char *str = va_arg(valist, char *);
 	char *hex;
 	int sum = 0;
 
@@ -78,17 +77,17 @@ int print_S(va_list valist, parameters_t *parameter)
  */
 int print_unsigned(va_list valist, parameters_t *parameter)
 {
-    unsigned long l;
+	unsigned long l;
+	
+	if (parameter->l_modifier)
+		l = (unsigned long)va_arg(valist, long);
+	else if (parameter->h_modifier)
+		l = (unsigned short int)va_arg(valist, int);
+	else
+		l = (unsigned int)va_arg(valist, int);
 
-    if (parameter->l_modifier)
-	    l = (unsigned long)va_arg(valist, long);
-    else if (parameter->h_modifier)
-	    l = (unsigned short int)va_arg(valist, int);
-    else
-	    l = (unsigned int)va_arg(valist, int);
-
-    parameter->sign = 1;
-    return (print_number(convert(l, 10, CONVERT_UNSIGNED, parameter), parameter));
+	parameter->sign = 1;
+	return (print_number(convert(l, 10, CONVERT_UNSIGNED, parameter), parameter));
 }
 
 /**
@@ -100,14 +99,13 @@ int print_unsigned(va_list valist, parameters_t *parameter)
  */
 int print_address(va_list valist, parameters_t *parameter)
 {
-    unsigned long int n = va_arg(valist, unsigned long int);
-    char *s;
-
-    if (!n)
-	    return (_puts("(nil)"));
-
-    s = convert(n, 16, CONVERT_LOWERCASE | CONVERT_UNSIGNED, parameter);
-    *--s = 'x';
-    *--s = '0';
-    return (print_number(s, parameter));
+	unsigned long int n = va_arg(valist, unsigned long int);
+	char *s;
+	
+	if (!n)
+		return (_puts("(nil)"));
+	s = convert(n, 16, CONVERT_LOWERCASE | CONVERT_UNSIGNED, parameter);
+	*--s = 'x';
+	*--s = '0';
+	return (print_number(s, parameter));
 }
