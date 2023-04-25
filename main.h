c754353f4b29736853fe499b3cb22043021eca8c
xxyouszz <125680172+xxyouszz@users.noreply.github.com>
2023-04-25 17:14:45 +0200

    Update main.h

--- main.h
+++ main.h
@@ -17,4 +17,24 @@ int _putchar(char c);
 //numbers.c
 char *convert(long int num, int base);
 
+//parameters.c
+typedef struct parameters
+{
+    unsigned int sign           : 1;
+
+    unsigned int plusFlag       : 1;
+    unsigned int minusFlag      : 1;
+    unsigned int hashFlag       : 1;
+    unsigned int spaceFlag      : 1;
+    unsigned int zeroFlag       : 1;
+
+    unsigned int precision;
+    unsigned int width;
+
+    unsigned int l_modifier     : 1;
+    unsigned int h_modifier     : 1;
+} parameters_t;
+void init_param(parameters_t *parameter, va_list valist)
+int get_flag(char *s, parameters_t *parameter)
+
 #endif /* MAIN_H */
