1196c72375ecdd4aa6556ad0c813f6f26d211e25
xxyouszz <125680172+xxyouszz@users.noreply.github.com>
2023-04-25 17:25:40 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -14,7 +14,9 @@ int _printf(const char *format, ...)
 	parameters_t parameters = PARAM_INITIALS;
 
 	va_start(valist, format);
-
+	
+        if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	if (!format || format[0] == '%' && format[1] == '\0')
 		return (-1);
 	for(p = (char *)format; *p; p++)
@@ -26,6 +28,21 @@ int _printf(const char *format, ...)
 		}
 		start = p;
 		p++;
+		while (get_flag(p, &params)) /* while char at p is flag character */
+		{
+			p++; /* next character */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+					params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
+	_putchar(BUF_FLUSH);
 		
 	}
 
